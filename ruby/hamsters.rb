27d5dddd4db31bc2d38cc9eeb4bafc31b581cdd0
arseny010 <avsarsen@gmail.com>
2016-11-21 16:35:18 -0600

    Add files via upload

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "name of the hamster"
+name = gets.chomp
+
+puts "volume level"
+name = gets.chomp
+
+puts "fur color"
+name = gets.chomp
+
+puts "Is a candidate good for adoption"
+name = gets.chomp
+
+puts "estimated age"
+name = gets.chomp
