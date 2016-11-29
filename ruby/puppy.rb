e138eb705138d27cbe39589abe28d157790afe1f
arseny010 <avsarsen@gmail.com>
2016-11-28 21:42:08 -0600

    Add files via upload

---
+++ ruby/puppy.rb
@@ -0,0 +1,35 @@
+puppy class
+wolfies
+
+
+species----------------
+smirty whoffer
+
+characteristic------------
+Pows length: 1-1.2 ft
+Friendly: yes
+Tail count: one
+Name: varies
+Color: varies
+
+Behavior------------
+bark
+lick
+run
+
+class Puppy
+ 
+	end
+ duchess = Puppy.new
+	fido = Puppy.new
+	spot = Puppy.new
+	
+	 spot.class
+	 
+	 str = String.new
+	 str = "yoho-ho"
+	 str.length
+   str += "huh."
+   greeting = String.new("hello")
+   greeting.length
+   greeting.upcase
